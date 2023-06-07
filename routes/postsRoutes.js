8d941c707edb3ac6202cfe14a64b83a9fe121e20
billygotclout <williamsoluwademilade@gmail.com>
2023-06-07 18:08:33 +0100

    made changes

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer")
 const { getPosts, createPost, getPost, updatePost, deletePost } = require("../controllers/postsController");
 const validateToken = require("../middleware/validateToken");
-const { like } = require("../controllers/likesController");
+
 const upload = multer({dest:"uploads/"})
 
 const router = express.Router()
@@ -13,6 +13,6 @@ router.route("/create-post").post(upload.single("post_image"),createPost)
 router.route("/get-post/:id").get(getPost)
 router.route("/update-post/:id").put(updatePost)
 router.route("/delete-post/:id").delete(deletePost)
-router.route("/likes/:id").get(like)
+
 
 module.exports=router
